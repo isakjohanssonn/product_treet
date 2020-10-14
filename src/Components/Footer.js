77fcb056e6668dc235246f92966d5f9f52d59bfa
Oscar Olsson <oscol968@student.liu.se>
2020-10-14 17:37:57 +0200

    navbar-header-fix

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,30 +2,31 @@ import React, { Component } from 'react';
 import { ListGroup, Button } from "react-bootstrap";
 import { Link, Router } from "react-router-dom";
 import "./Footer.css"
+import "./Navigationbar"
+import {globaltitle} from './Navigationbar';
 
 
 export const Footer = () => {
+  globaltitle = "råtta";
   return (
     <div>
-
       <div className="style">
         <ListGroup horizontal>
-          <ListGroup.Item action tolink={"/"} ><Link to="/">H</Link>
+          <Link to="/"><span><ListGroup.Item action >Home
+          </ListGroup.Item></span></Link>
 
-          </ListGroup.Item>
           <Link to="/"><span><ListGroup.Item action >H
-
           </ListGroup.Item></span></Link>
-          <Link to="/MedicalData"><span><ListGroup.Item action >H
 
+          <Link to="/MedicalData"><span><ListGroup.Item action >History
           </ListGroup.Item></span></Link>
-          <ListGroup.Item action ><Link to="/contact">M</Link>
 
-          </ListGroup.Item>
-          <ListGroup.Item action ><Link to="/Profile">P</Link>
+          <Link to="/contact"><span><ListGroup.Item action >help
+          </ListGroup.Item></span></Link>
 
-          </ListGroup.Item>
-          
+          <Link to="/profile"><span><ListGroup.Item action >P
+          </ListGroup.Item></span></Link>
+
         </ListGroup>,
         </div>
 
--- src/Components/Navigationbar.js
+++ src/Components/Navigationbar.js
@@ -1,40 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Nav, Navbar, Form } from 'react-bootstrap';
 import { Link, withRouter } from "react-router-dom";
 import "./Navigationbar.css";
 
 
+var globaltitle = "";
 
 
+const NavigationBar = () => {
+  const [title, setTitle] = useState();
+  globaltitle = title;
+  useEffect(() => {
+    document.title = globaltitle;
+  });
+  return (
+    <div>
+      <Navbar expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Form className="form-center">
+        </Form>
+        <navbar-brand ID='NavbarID'> {title}</navbar-brand>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Link to="/" onClick={() => setTitle("Home")}><span><Nav.Item><div>Home</div></Nav.Item></span></Link>
+            <Link to="/Login" onClick={() => setTitle("Login")}><span><Nav.Item><div>Log in</div></Nav.Item></span></Link>
+            <Link to="/MedicalData" onClick={() => setTitle("MedicalData")}><span><Nav.Item><div>History</div></Nav.Item></span></Link>
+            <Link to="/profile" onClick={() => setTitle("Profile")}><span><Nav.Item><div>profile</div></Nav.Item></span></Link>
 
-class NavigationBar extends Component {
-  state = {
-    title: "Home"
-  }
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 
-
-  render() {
-    return (
-      <div>
-        <Navbar expand="lg">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Form className="form-center">
-          </Form>
-          <navbar-brand ID='NavbarID'> {this.state.title}</navbar-brand>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Link to="/"><span><Nav.Item><div>Home</div></Nav.Item></span></Link>
-              <Link to="/Login"><span><Nav.Item><div>Log in</div></Nav.Item></span></Link>
-              <Link to="/MedicalData"><span><Nav.Item><div>History</div></Nav.Item></span></Link>
-              <Link to="/profile"><span><Nav.Item><div>profile</div></Nav.Item></span></Link>
-
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
-  }
 }
-
 
 export default NavigationBar;
