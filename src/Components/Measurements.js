ffdced12f7e19734d8405e9637aa786a4168fd3c
sinwo612 <sinwo612@student.liu.se>
2020-10-28 14:29:30 +0100

    Top buttons in measurements view

--- src/Components/Measurements.js
+++ src/Components/Measurements.js
@@ -1,15 +1,30 @@
 import React from "react";
-//import "./Measurements.css"
+import "./Measurements.css";
+import Dropdown from 'react-bootstrap/Dropdown';
+import Button from 'react-bootstrap/Button';
 
-export default function Streak() {
+export default function Measurements() {
 
   return (
-    <div className="mainStreakDiv">
+    <div>
       <div className="header">
         <h3>Measurements</h3>
       </div>
+        <div className="topButtons">
+        <Dropdown>
+            <Dropdown.Toggle className="topButton" id="dropdown-basic">
+                Type
+            </Dropdown.Toggle>
 
-
+            <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+        
+        <Button className="topButton" id='allMeasurements'>All measurements</Button>
+        </div>
     </div>
   );
 }
