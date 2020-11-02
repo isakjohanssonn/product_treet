d5ca6ac3d6c040e522af906a532f24eeb86e3d0f
Oscar Olsson <oscol968@student.liu.se>
2020-11-02 12:02:36 +0100

    fix

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -1,41 +1,40 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Button, Overlay, Popover } from 'react-bootstrap';
-//import {IoIosNotifications} from 'react-icons/io'
+import {IoIosNotifications} from 'react-icons/io'
 
 
 const Notifications = () => {
 
 
-    // const [show, setShow] = useState(false);
-    // const [target, setTarget] = useState(null);
-    // const ref = useRef(null);
+    const [show, setShow] = useState(false);
+    const [target, setTarget] = useState(null);
+    const ref = useRef(null);
   
-    // const handleClick = (event) => {
-    //   setShow(!show);
-    //   setTarget(event.target);
-    // };
+    const handleClick = (event) => {
+      setShow(!show);
+      setTarget(event.target);
+    };
   
     return (
-      // <div ref={ref}>
-      //   <Button onClick={handleClick }></Button>
-      //   {/* <IoIosNotifications></IoIosNotifications> */}
+      <div ref={ref}>
+        <Button onClick={handleClick }><IoIosNotifications></IoIosNotifications></Button>
+  
 
-      //   <Overlay
-      //     show={show}
-      //     target={target}
-      //     placement="left-end"
-      //     container={ref.current}
-      //     containerPadding={20}
-      //   >
-      //     <Popover id="popover-contained">
-      //       <Popover.Title as="h3">Du har 5 timmar att mäta din aktivitet innan din streak går ut</Popover.Title>
-      //       <Popover.Content>
-      //         <strong>Glöm inte</strong> att mäta dina värden.
-      //       </Popover.Content>
-      //     </Popover>
-      //   </Overlay>
-      // </div>
-      <div>hello</div>
+        <Overlay
+          show={show}
+          target={target}
+          placement="left-end"
+          container={ref.current}
+          containerPadding={20}
+        >
+          <Popover id="popover-contained">
+            <Popover.Title as="h3">Du har 5 timmar att mäta din aktivitet innan din streak går ut</Popover.Title>
+            <Popover.Content>
+              <strong>Glöm inte</strong> att mäta dina värden.
+            </Popover.Content>
+          </Popover>
+        </Overlay>
+      </div>
     );
   
   
