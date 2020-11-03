5ec7d90ae7afb6e6e275ef64789d2430aeef9c46
Oscar Olsson <oscol968@student.liu.se>
2020-11-03 18:51:30 +0100

    Navbar looks decent

--- src/Components/Navigationbar.js
+++ src/Components/Navigationbar.js
@@ -1,34 +1,66 @@
 import React, { useState, useEffect } from 'react';
 import { Nav, Navbar, Form } from 'react-bootstrap';
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Navigationbar.css";
 import Notifications from "./Notifications";
 
 
 const NavigationBar = (props) => {
+  const { globalTitle, setGlobalTitle } = props;
+  const [expanded, setExpanded] = useState(false);
+  const [activeHome, setActiveHome] = useState("active");
+  const [activeLogin, setActiveLogin] = useState("");
+  const [activeMedData, setActiveMedData] = useState("");
+  const [activeProfile, setActiveProfile] = useState("");
+  const [activeMeasurements, setActiveMeasurements] = useState("");
 
-  const {globalTitle, setGlobalTitle} = props;
+  useEffect(() => {
+    if (globalTitle == "Home") {
+      setActiveHome("active");
+    } else { setActiveHome(""); }
+    
+    if (globalTitle == "Login") {
+      setActiveLogin("active");
+    } else { setActiveLogin(""); }
+
+    if (globalTitle == "Medical data") {
+      setActiveMedData("active");
+    } else { setActiveMedData(""); }
+
+
+    if (globalTitle == "Profile") {
+      setActiveProfile("active");
+    } else { setActiveProfile(""); }
+    
+    if (globalTitle == "Measurements") {
+      setActiveMeasurements("active");
+    } else { setActiveMeasurements(""); }
+    
+
+
+  }, [globalTitle])
 
   return (
     <div>
-      <Navbar expand="lg">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Form className="form-center"> 
-        </Form>
-        <navbar-brand ID='NavbarID'> {globalTitle}</navbar-brand>
+      <Navbar expanded={expanded} expand="lg">
+        <Navbar.Toggle onClick={() => setExpanded(expanded ? false : true)} />
+        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        <Form className="form-center"> {globalTitle} </Form>
+        <navbar-brand ID='NavbarID'>  </navbar-brand>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Link to="/" onClick={() => setGlobalTitle("Home")}><span><Nav.Item><div>Home</div></Nav.Item></span></Link>
-            <Link to="/Login" onClick={() => setGlobalTitle("Login")}><span><Nav.Item><div>Log in</div></Nav.Item></span></Link>
-            <Link to="/MedicalData" onClick={() => setGlobalTitle("Medical data")}><span><Nav.Item><div>History</div></Nav.Item></span></Link>
-            <Link to="/profile" onClick={() => setGlobalTitle("Profile")}><span><Nav.Item><div>profile</div></Nav.Item></span></Link>
-            <Link to="/measurements" onClick={() => setGlobalTitle("Measurements")}><span><Nav.Item><div>Measurements</div></Nav.Item></span></Link>
+          <Nav data-toggle="collapse" className="ml-auto">
+            <Link id={activeHome} className="navLink" to="/" onClick={() => { setExpanded(false); setGlobalTitle("Home") }} ><span ><Nav.Item >Home</Nav.Item></span></Link>
+            <Link id={activeLogin} className="navLink" to="/Login" onClick={() => { setExpanded(false); setGlobalTitle("Login") }}><span><Nav.Item ><div>Log in</div></Nav.Item></span></Link>
+            <Link id={activeMedData} className="navLink" to="/MedicalData" onClick={() => { setExpanded(false); setGlobalTitle("Medical data") }}><span><Nav.Item><div>History</div></Nav.Item></span></Link>
+            <Link id={activeProfile} className="navLink" to="/profile" onClick={() => { setExpanded(false); setGlobalTitle("Profile") }}><span><Nav.Item><div>profile</div></Nav.Item></span></Link>
+            <Link id={activeMeasurements} className="navLink" to="/measurements" onClick={() => { setExpanded(false); setGlobalTitle("Measurements") }}><span><Nav.Item><div>Measurements</div></Nav.Item></span></Link>
 
           </Nav>
         </Navbar.Collapse>
-        <Notifications />
+        {(!expanded &&
+        <Notifications />)}
       </Navbar>
-    </div>
+    </div >
   );
 
 }
