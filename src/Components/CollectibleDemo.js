a9d7469fdf476e9be87e74b4d63ac982425fe30c
sinwo612 <sinwo612@student.liu.se>
2020-11-05 11:12:40 +0100

    CollectibleMedal component created

--- src/Components/CollectibleDemo.js
+++ src/Components/CollectibleDemo.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./CollectibleDemo.css";
 import CollectibleRibbon from "./CollectibleRibbon.js";
+import CollectibleMedal from "./CollectibleMedal.js";
 
 const CollectibleDemo = () => {
 
   return (
-    <div>
-      <CollectibleRibbon/>
+    <div class="collectible-demos">
+      <CollectibleRibbon number={5}/>
+      <CollectibleMedal number={10}/>
 
     </div>
   );
