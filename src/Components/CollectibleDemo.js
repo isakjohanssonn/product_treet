caa7e5d49723dd8450438bcfd6419a1833f552cb
Fredrik Thorsson <fredrikthorsson@Fredriks-MacBook-Air.local>
2020-11-05 10:14:19 +0100

    initial collectible demos

---
+++ src/Components/CollectibleDemo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CollectibleDemo.css";
+import CollectibleRibbon from "./CollectibleRibbon.js";
+
+const CollectibleDemo = () => {
+
+  return (
+    <div>
+      <CollectibleRibbon/>
+
+    </div>
+  );
+}
+
+export default CollectibleDemo;
---
+++ src/Components/CollectibleRibbon.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { GiRibbonMedal } from 'react-icons/gi';
+
+const CollectibleRibbon = () => {
+
+  return (
+    <div class = "ribbon">
+      <GiRibbonMedal size={60} color={"red"}/>
+        5
+    </div>
+  );
+}
+
+export default CollectibleRibbon;
