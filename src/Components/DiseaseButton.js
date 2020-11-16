9eef3c40c786f85ff570300ab217b3d033b9f85c
Uno Österman <unoos538@student.liu.se>
2020-11-16 15:16:38 +0100

    Added more buttons and fixed links for goals page, fixed formation in goals.css and design with new navbar comp

--- src/Components/DiseaseButton.js
+++ src/Components/DiseaseButton.js
@@ -1,30 +1,40 @@
 import React, { useState } from "react";
+import Button from 'react-bootstrap/Button';
 import { Card } from "react-bootstrap";
-import "./Disease.css";
+import "./DiseaseButton.css";
 import { ArrowRight, Box } from 'react-bootstrap-icons'; 
 import GetDemographicsEHR from "../openEHR/GetDemographicsEHR.js";
+import {Link} from 'react-router-dom';
 
 // If the arrow is not displayed (The one from bootstrap icons)
 // Try installing bootstrap icons in the terminal with the command "npm install react-bootstrap-icons --save"
 
-const DiseaseButton = () => {
+const DiseaseButton = (props) => {
   const [person] = useState(GetDemographicsEHR)
+  if (props.title == 'Diabetes') {
   return (
-    <div>
-
-
-      <div>
-        {/* Creating the card/box to contain the achievements */}
-        <Card className="diseaseButtonCard" >
-          <Card.Header className="diseaseButtonCardHeader">Your latest achievements</Card.Header>
+    <div>     
+       <Link to='/Diabetes'>    
+        <Card  className="diseaseButtonCard" id='firstButton'>
+          <Card.Header className="diseaseButtonCardHeader">{props.title}</Card.Header>
           <Card.Body className="diseaseButtonCardBody card-columns">
-
           </Card.Body>
         </Card>
-      </div>
-
-
+        </Link>
       </div>
       );
+   } else {
+      return (
+      <div>
+      {/* <Link to='/'>  */}
+      <Card  className="diseaseButtonCard">
+      <Card.Header className="diseaseButtonCardHeader">{props.title}</Card.Header>
+      <Card.Body className="diseaseButtonCardBody card-columns">
+      </Card.Body>
+      </Card>
+      {/* </Link>  */}
+      </div>
+      );
+     }
 }
 export default DiseaseButton;
--- src/Goals.js
+++ src/Goals.js
@@ -1,14 +1,23 @@
 import React from "react";
-
-import Navigationbar from './Components/Navigationbar'
-import Footer from './Components/Footer'
+import "./Goals.css";
+import DiseaseButton from './Components/DiseaseButton';
+import Navigationbar from './Components/Navigationbar';
+import Footer from './Components/Footer';
 
 const GoalsPage = () => {
   return (
+
     <div>
       <Navigationbar title={'Goals'} />
+      <div>
+      <DiseaseButton title={'Diabetes'} link={'/Diabetes'} />
+      <DiseaseButton title={'Heartfailure'}/>
+      <DiseaseButton title={'COPD (KOL)'}/>
+      <DiseaseButton title={'Hypertoni'}/>
+      </div>
       <Footer />
-    </div>
+ 
+   </div>
   );
 }
  
