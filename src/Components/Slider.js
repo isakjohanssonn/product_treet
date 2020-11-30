68ab993073b979bf04c0b532e6a2ef7f67ae32d3
sinwo612 <sinwo612@student.liu.se>
2020-11-30 13:12:31 +0100

    Picture in slider. Positioning of slider buttons

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import "./Slider.css";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
+import { getTree } from "./AllTrees";
 
 const Slider = () => {
 
   return (
     <div className="slider">
         <Link to="/forest" className="slider-link">
-            <Button className="slider-button">Forest</Button>
+            <Button className="slider-button">
+              <object className="tree-image-slider" type="image/svg+xml" data={getTree(3, 7, false, false)}/>
+              Forest
+            </Button>
         </Link>
         <Link to="/achievement" className="slider-link">
             <Button className="slider-button">Achievements</Button>
