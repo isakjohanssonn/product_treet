599b1010f63176e958125d8a894467b74544f950
sinwo612 <sinwo612@student.liu.se>
2020-11-30 13:22:27 +0100

    Positioning of slider

--- src/Components/Slider.js
+++ src/Components/Slider.js
@@ -3,6 +3,7 @@ import "./Slider.css";
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
 import { getTree } from "./AllTrees";
+import placeholder from "../images/placeholderHexagon.png";
 
 const Slider = () => {
 
@@ -10,12 +11,19 @@ const Slider = () => {
     <div className="slider">
         <Link to="/forest" className="slider-link">
             <Button className="slider-button">
-              <object className="tree-image-slider" type="image/svg+xml" data={getTree(3, 7, false, false)}/>
+              <div className="image-holder">
+                <object className="tree-image-slider" type="image/svg+xml" data={getTree(3, 7, false, false)}/>
+              </div>
               Forest
             </Button>
         </Link>
         <Link to="/achievement" className="slider-link">
-            <Button className="slider-button">Achievements</Button>
+            <Button className="slider-button">
+              <div className="image-holder">
+                <object className="tree-image-slider" type="image/png" data={placeholder}/>
+              </div>
+              Achievements
+            </Button>
         </Link>
     </div>
   );
