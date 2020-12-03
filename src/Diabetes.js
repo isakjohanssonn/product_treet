af94e4b940347627f99f025bfd6385c5f7b753a8
Uno Österman <unoos538@student.liu.se>
2020-12-03 18:31:09 +0100

    Created the card boxes for latest and near achievements. However, had to hard code in the goals since there were no goals component and it is very intertwined with achievement. also added card css in the profilePage.css

--- src/Diabetes.js
+++ src/Diabetes.js
@@ -5,31 +5,50 @@ import Streak from "./Components/Streak";
 import Achievement from "./Components/Achievement";
 import ComingAchievementBox from "./Components/ComingAchievementBox";
 import { Card } from "react-bootstrap"
+import "./Global.css"
+import AchievementBox from "./Components/AchievementBox";
+import "./ProfilePage.css"
+import { ArrowRight } from 'react-bootstrap-icons';
+import BronzeStar from "./img/achievementsimg/bronze_star.png";
+import SilverStar from "./img/achievementsimg/silver_star.png";
+import GoldStar from "./img/achievementsimg/golden_star.png";
 
-const achievementLoop = (props) => {
-  const {level, reachedAchievements, currentStreak, longestStreak} = props 
-  if (level > 1) {
-    alert(reachedAchievements.lenght - 1)
-    return (
-      <Card>
-        <Card.Body>
-        <p>"WALLA"</p>
-        <ComingAchievementBox lastAchievement = {reachedAchievements[reachedAchievements.lenght - 1]} showOne = {true} currentStreak = {props.currentStreak} />
+export const GoalsBox = (props) => {
+  const { icon, title } = props;
+  return (
+    <div>
+      <Card className="boxCardMain " >
+        <Card.Body className="cardBody card-columns">
+          <div className="iconDiv">
+          <img src={icon} className="profileBoxIcon" />
+          </div>
+          <Card.Header className="cardHeader">{title}</Card.Header>
+          <div className="divCardArrow" >
+            <ArrowRight viewBox="0 0 16 16" className="pull-right cardArrow" fill="currentColor" />
+          </div>
         </Card.Body>
       </Card>
-    );
-  } 
-  
 
+    </div>
+  );
 }
 
 const DiabetesPage = (props) => {
+  const {reachedAchievements, currentStreak} = props 
   return (
     <div>
       <Navigationbar title={'Diabetes'} />
+      <div className="scrollable">
+      <div className = "topBlock"></div>
       <Streak {...props} />
-      {achievementLoop(props)}
-      
+      <Card.Header className="centeredCardTitle">Goals</Card.Header>
+      <GoalsBox title="Lose 5 kg" icon = {SilverStar}/>
+      <GoalsBox title="Lower avg blood pressure than previous week" icon = {BronzeStar}/>
+      <GoalsBox title="Consistent measurements 14 days in a row" icon = {GoldStar}/>
+      <Card.Header className="centeredCardTitle">Reached goals</Card.Header>
+      <GoalsBox title="Lose 2 kg" icon = {BronzeStar}/>
+      <GoalsBox title="Consistent measurements 7 days in a row" icon = {SilverStar}/>
+      </div>
       {/* Todo: Import streak
                 Create dud-buttons for goals
                 */}
