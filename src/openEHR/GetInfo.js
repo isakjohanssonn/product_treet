8284c0520316d91f4532c7cfc54d1e35c0feb45d
augka793 <augka793@student.liu.se>
2020-12-08 00:01:09 +0100

    Can now get arrays from EHR of any size.

--- src/openEHR/GetInfo.js
+++ src/openEHR/GetInfo.js
@@ -1,32 +1,37 @@
 const GetInfo = (setLevel, setCurrentStreak, setLongestStreak, setCurrentTree, setTreeAge, setIsSickTree, setReachedAchievement) => {
-    
-    var user = 'lio.se1';
-    var pw = 'lio.se123';
-    var url = 'https://rest.ehrscape.com/rest/v1/view/PUM_COMP4_GameSettings_v3';
-    var headers = new Headers();
-    headers.append('Authorization', 'Basic ' + btoa(user + ":" + pw));
-    
-    console.log("beginning of getinfo")
-    
-    fetch(url, {
-      method: 'GET',
-      headers: headers,
-    }).then(res => res.json()).then(res => {
-      console.log("all info är nu : ", res[0])
-      console.log("achievments är ", [parseInt(res[0]["achivements"][1]), parseInt(res[0]["achivements"][3])])//[parseInt(res[0]["achivements"][1]), parseInt(res[0]["achivements"][3])])
-      //var test = res[0]["achivements"];
-      //console.log(parseInt(res[0]["currentTree"]))
 
-      setLevel(parseInt(res[0]["gameLevel"]))
-      setCurrentStreak(parseInt(res[0]["currentStreak"]))
-      setLongestStreak(parseInt(res[0]["longestStreak"]))
-      setCurrentTree(parseInt(res[0]["currentTree"]))
-      setTreeAge(parseInt(res[0]["treeAge"]))
-      setIsSickTree(res[0]["isSick"])
-      setReachedAchievement([parseInt(res[0]["achivements"][1]), parseInt(res[0]["achivements"][3])])
+  var user = 'lio.se1';
+  var pw = 'lio.se123';
+  var url = 'https://rest.ehrscape.com/rest/v1/view/PUM_COMP4_GameSettings_v3';
+  var headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa(user + ":" + pw));
 
-    });
-    //console.log("end of getinfo")
-} 
+  fetch(url, {
+    method: 'GET',
+    headers: headers,
+  }).then(res => res.json()).then(res => {
+    setLevel(parseInt(res[0]["gameLevel"]))
+    setCurrentStreak(parseInt(res[0]["currentStreak"]))
+    setLongestStreak(parseInt(res[0]["longestStreak"]))
+    setCurrentTree(parseInt(res[0]["currentTree"]))
+    setTreeAge(parseInt(res[0]["treeAge"]))
+    setIsSickTree(res[0]["isSick"])
+    setReachedAchievement(() => {
+      let x = [];
+      let val = "";
+      for (let i = 1; i < (res[0]["achivements"]).length - 1; i++) {
+        if (res[0]["achivements"][i] == ',') {
+          x.push(parseInt(val));
+          val = "";
+        }
+        else {
+          val = val.concat(res[0]["achivements"][i]);
+        }
+      }
+      x.push(parseInt(val));
+      return x;
+    })
+  });
+}
 
 export default GetInfo;
--- src/openEHR/PostInfo.js
+++ src/openEHR/PostInfo.js
@@ -1,37 +1,31 @@
 import { JournalArrowDown } from "react-bootstrap-icons";
 
 const PostInfo = (level, currentstreak, longeststreak, currenttree, treeage, issick, achievment) => {
-    
-    var user = 'cds@lio.se';
-    var pw = 'cds4ehr';
-    var url = 'https://rest.ehrscape.com/rest/v1/composition?ehrId=6a0ed4a1-ddd5-47ce-8ffa-ae4a93de5eae&templateId=PUM_COMP4_gameSettings_v1&format=FLAT';
-    var headers = new Headers();
-    headers.append('Authorization', 'Basic ' + btoa(user + ":" + pw));
-    headers.append('Content-Type', 'application/json');
-    console.log("beginning of postinfo")
-    console.log("achievment", achievment);
 
+  var user = 'cds@lio.se';
+  var pw = 'cds4ehr';
+  var url = 'https://rest.ehrscape.com/rest/v1/composition?ehrId=6a0ed4a1-ddd5-47ce-8ffa-ae4a93de5eae&templateId=PUM_COMP4_gameSettings_v1&format=FLAT';
+  var headers = new Headers();
+  headers.append('Authorization', 'Basic ' + btoa(user + ":" + pw));
+  headers.append('Content-Type', 'application/json');
 
-    const data = {
-      "ctx/language": "en",
-      "ctx/territory": "US",
-      "pum_comp4_gamesettings_v1/gamification/gameificationlevel": JSON.stringify(level),
-      "pum_comp4_gamesettings_v1/gamification/currentstreak": JSON.stringify(currentstreak),
-      "pum_comp4_gamesettings_v1/gamification/longeststreak": JSON.stringify(longeststreak),
-      "pum_comp4_gamesettings_v1/gamification/currenttree": JSON.stringify(currenttree),
-      "pum_comp4_gamesettings_v1/gamification/treeage": JSON.stringify(treeage),
-      "pum_comp4_gamesettings_v1/gamification/issick": issick,
-      "pum_comp4_gamesettings_v1/gamification/achivement:0": JSON.stringify(achievment)//[1, 2, 3, 4]  //Achievment, en lista av integers JSON.stringify(achievment)
-    };
+  const data = {
+    "ctx/language": "en",
+    "ctx/territory": "US",
+    "pum_comp4_gamesettings_v1/gamification/gameificationlevel": JSON.stringify(level),
+    "pum_comp4_gamesettings_v1/gamification/currentstreak": JSON.stringify(currentstreak),
+    "pum_comp4_gamesettings_v1/gamification/longeststreak": JSON.stringify(longeststreak),
+    "pum_comp4_gamesettings_v1/gamification/currenttree": JSON.stringify(currenttree),
+    "pum_comp4_gamesettings_v1/gamification/treeage": JSON.stringify(treeage),
+    "pum_comp4_gamesettings_v1/gamification/issick": issick,
+    "pum_comp4_gamesettings_v1/gamification/achivement:0": JSON.stringify(achievment)
+  };
 
-    
-
-    fetch(url, {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(data)
-    });// }).then(res => console.log(res.json()));
-    console.log("end of postinfo")
-} 
+  fetch(url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data)
+  });
+}
 
 export default PostInfo;
